9752a51055f98d7c445e6d509da5db78b1401d37
Antonella Castillo <ant.castillos@duocuc.cl>
2026-06-30 01:29:19 -0400

    feat: se agregó la clase pago rechazado como una excepcion

--- src/main/java/com/perfulandia/ms_pagos/exception/PagoRechazadoException.java
+++ src/main/java/com/perfulandia/ms_pagos/exception/PagoRechazadoException.java
@@ -1,5 +1,11 @@
 package com.perfulandia.ms_pagos.exception;
 
-public class PagoRechazadoException {
-
+// EXCEPCIÓN PROPIA (KAN-23): cuando un pago no se puede procesar.
+// El handler la convierte en un 402 Payment Required.
+public class PagoRechazadoException extends RuntimeException 
+{
+    public PagoRechazadoException(String mensaje) 
+    {
+        super(mensaje);
+    }
 }
