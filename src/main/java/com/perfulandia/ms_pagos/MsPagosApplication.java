29c2e2dee483fad86e3b4362c91da92bd4ac0c3c
Antonella Castillo <ant.castillos@duocuc.cl>
2026-06-30 01:09:06 -0400

    feat: clase ms pagos application ajustada

--- src/main/java/com/perfulandia/ms_pagos/MsPagosApplication.java
+++ src/main/java/com/perfulandia/ms_pagos/MsPagosApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsPagosApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MsPagosApplication.class, args);
-	}
-
+public class MsPagosApplication 
+{
+    public static void main(String[] args) {
+        SpringApplication.run(MsPagosApplication.class, args);
+    }
 }
