162c4b111301392fa70024abb1b41bf2b470d57d
Antonella Castillo <ant.castillos@duocuc.cl>
2026-07-03 01:54:12 -0400

    fix: ajustes pequeños

--- src/test/java/com/perfulandia/ms_pagos/service/NotificacionClienteServiceTest.java
+++ src/test/java/com/perfulandia/ms_pagos/service/NotificacionClienteServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 // Prueba del NotificacionClienteService (comunicación REST resiliente con MS Notificaciones).
